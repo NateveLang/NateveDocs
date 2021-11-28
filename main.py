0fae93775595163d4a1392c0162ed54ac2bd0561
eanorambuena <eanorambuena@uc.cl>
2021-11-28 05:16:32 -0300

    Update m2r  transformation

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import subprocess
+import subprocess, os
 
 file = "README"
 
 subprocess.call([".\m2r", file + ".md"])
+
+f = open(file + ".rst")
+text = f.read()
+f.close()
+
+index = open("index.rst", "w")
+index.write(text)
+index.close()
+
